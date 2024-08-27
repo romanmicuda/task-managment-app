92e9411ed3ba1137b28505da6104172621a666ac
Roman Micuda <roman.micuda@outlook.com>
2024-08-27 10:08:50 +0200

    feat: display side bar

--- src/component/SideBar.tsx
+++ src/component/SideBar.tsx
@@ -1,18 +1,127 @@
-import React from "react";
+import React, { useDebugValue, useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { useTodo } from "./TodoProvider";
+import { Group } from "../types";
 
-function SideBar() {
+const SideBar = () => {
+  const { groups, setGroups } = useTodo();
+  const [expandedGroup, setExpandedGroup] = useState<Set<string>>(new Set());
+  const [newListName, setNewListName] = useState<string>("");
+  const [newListGroupName, setNewListGroupName] = useState<string>("");
+  const [newGroupName, setNewGroupName] = useState<string>("");
+
+  const handleExpandedGroup = (groupName: string) => {
+    if (expandedGroup.has(groupName)) {
+      setExpandedGroup((prevExpandedGroup) => {
+        const newExpanedGroup = new Set(prevExpandedGroup);
+        newExpanedGroup.delete(groupName);
+        return newExpanedGroup;
+      });
+    } else {
+      setExpandedGroup((prevExpandedGroup) => {
+        return new Set(prevExpandedGroup).add(groupName);
+      });
+    }
+  };
+
+  const handleAddGroup = () => {
+    setGroups((prevGroups) => [
+      ...prevGroups,
+      { name: newGroupName, groupList: [] },
+    ]);
+  };
+
+  const handleAddList = () => {
+    setGroups((prevGroup) => [
+      ...prevGroup.map((group) =>
+        group.name === newListGroupName
+          ? {
+              name: group.name,
+              groupList: [...group.groupList, { name: newListName, lists: [] }],
+            }
+          : group
+      ),
+    ]);
+    setNewListGroupName("");
+    setNewListName("");
+  };
+
   return (
     <div>
-      <ul>
-        <Link to={"/myday"}>My day</Link>
-        <Link to={"/important"}>Important</Link>
-        <Link to={"/planned"}>Planned</Link>
-        <Link to={"/tasks"}>Tasks</Link>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/myday"}>My day</Link>
+          </li>
+          <li>
+            <Link to={"/important"}>Important</Link>
+          </li>
+          <li>
+            <Link to={"/planned"}>Planned</Link>
+          </li>
+          <li>
+            <Link to={"/tasks"}>Tasks</Link>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+          {groups.map((group) => (
+            <li
+              key={group.name}
+              onClick={() => handleExpandedGroup(group.name)}
+            >
+              {group.name}
+              <ul>
+                {expandedGroup.has(group.name) &&
+                  group.groupList.map((groupList) => (
+                    <li>
+                      <Link to={`list/${groupList.name}`}>
+                        {groupList.name}
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div>
+        <div>
+          <input
+            type="text"
+            value={newListName}
+            onChange={(e) => setNewListName(e.target.value)}
+            placeholder="Enter a new list"
+          />
+          <select
+            value={newListGroupName}
+            onChange={(e) => setNewListGroupName(e.target.value)}
+          >
+            <option value="" disabled>
+              Default
+            </option>
+            {groups.map((group) => (
+              <option value={group.name} key={group.name}>
+                {group.name}
+              </option>
+            ))}
+          </select>
+          <button onClick={() => handleAddList()}>Add List</button>
+        </div>
+        <div>
+          <input
+            type="text"
+            value={newGroupName}
+            onChange={(e) => setNewGroupName(e.target.value)}
+            placeholder="Enter a new group"
+          />
+          <button onClick={handleAddGroup}>Add group</button>
+        </div>
+      </div>
       <Outlet />
     </div>
   );
-}
+};
 
 export default SideBar;
